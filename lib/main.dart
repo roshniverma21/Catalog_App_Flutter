403b61e3c83b20c100bd3ffde9a6dcf7b8ada833
Roshni Verma <roshniverma@Roshnis-MacBook-Pro.local>
2022-12-20 10:43:06 +0530

    [Roshni] Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+             child: Text("welcome to rosh mall"),
+          ),
+        ),
+      ),
+    );
+  }
+}
