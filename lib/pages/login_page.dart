9ee0d22f93a00832c9faa73aea732fc496776d19
roshniverma021 <roshni.verma@thoughtworks.com>
2022-12-29 12:16:03 +0530

    [Roshni] Animation and futureDelay in button and stateful widgets

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -42,11 +50,17 @@ class LoginPage extends StatelessWidget {
 
             Padding(
               padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 20),
+              const EdgeInsets.symmetric(vertical: 16.0, horizontal: 20),
               child: Column(children: [
                 TextFormField(
                   decoration: const InputDecoration(
                       hintText: "Enter username", labelText: 'Username'),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {
+                      //this will call build method again and UI will change
+                    });
+                  },
                 ),
                 TextFormField(
                   obscureText: true,
@@ -56,13 +70,46 @@ class LoginPage extends StatelessWidget {
               ]),
             ),
 
-            ElevatedButton(
-              child: Text("login"),
-              style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-              onPressed: () {
+            //button with animation
+//container does not have clickable property so we have to wrap them with 1. Inkwell (it has a ripple effect means jab press kare toh pta laeg button press hua ) and ink and inkwell are used tor 2. Just a detector(it does not give any feedback and have no UI/ripple effect/ tap ni diktat)
+            InkWell(
+              onTap: () async {
+                setState(() {
+                  changeButton = true;
+                });
+                await Future.delayed(Duration(seconds: 1));
                 Navigator.pushNamed(context, MyRoute.homeRoute);
               },
-            )
+              child: AnimatedContainer(
+                duration: Duration(seconds: 1),
+                width: changeButton ? 50 : 150,
+                height: 50,
+                alignment: Alignment.center,
+                child: changeButton
+                    ? Icon(
+                  Icons.done,
+                  color: Colors.white,
+                )
+                    : Text(
+                  "Login",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(changeButton ? 50 : 8),
+                ),
+              ),
+            ),
+            // ElevatedButton(
+            //   child: Text("login"),
+            //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+            //   onPressed: () {
+            //     Navigator.pushNamed(context, MyRoute.homeRoute);
+            //   },
+            // )
           ],
         ),
       ),
