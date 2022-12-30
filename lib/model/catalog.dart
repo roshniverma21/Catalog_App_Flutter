5fd981de2b6e6350635ea3a60c6f1e6aacb5a000
roshniverma021 <roshni.verma@thoughtworks.com>
2022-12-30 11:34:24 +0530

    [Roshni] Created model for catalog page

---
+++ lib/model/catalog.dart
@@ -0,0 +1,23 @@
+class Item{
+  //final means this can't be changed
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});//curl;y braces m,ake them named parameters means yeh required honge hamesha
+
+  final products = [
+    Item(
+        id: "Codepur001",
+        name: "iPhone 12 Pro",
+        desc: "Apple iPhone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+  ];
+}
+
