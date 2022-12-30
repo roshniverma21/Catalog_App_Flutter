49fd104c44cc1d3f39fe8c6fdfdee50e8fb12fd1
roshniverma021 <roshni.verma@thoughtworks.com>
2022-12-30 13:01:56 +0530

    [Roshni ] Generate list and add to cards, asserts theory

--- lib/model/catalog.dart
+++ lib/model/catalog.dart
@@ -1,15 +1,5 @@
-class Item{
-  //final means this can't be changed
-  final String id;
-  final String name;
-  final String desc;
-  final num price;
-  final String color;
-  final String image;
-
-  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});//curl;y braces m,ake them named parameters means yeh required honge hamesha
-
-  final products = [
+class CatalogModel{
+  static  final items = [
     Item(
         id: "Codepur001",
         name: "iPhone 12 Pro",
@@ -19,5 +9,19 @@ class Item{
         image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
   ];
+}
+
+class Item{
+  //final means this can't be changed
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+   //curl;y braces m,ake them named parameters means yeh required honge hamesha
+
 }
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
 
+import '../model/catalog.dart';
 import '../widgets/drawer.dart';
+import '../widgets/items_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(30, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Page")
+        title: Text("Catalog App"),
       ),
-      body : const Center(
-        child: Text("Hi"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
-      drawer: MyDrawer()
-      );
+      drawer: MyDrawer(),
+    );
   }
 }
 
