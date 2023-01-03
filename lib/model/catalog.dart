c1837305597299d1cff79721217239c33bf3edef
roshniverma021 <roshni.verma@thoughtworks.com>
2023-01-03 11:03:08 +0530

    [Roshni] JSON mapping and data class Generator

--- lib/model/catalog.dart
+++ lib/model/catalog.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class CatalogModel{
-  static  final items = [
+  static List<Item> items = [
     Item(
-        id: "Codepur001",
+        id: 1,
         name: "iPhone 12 Pro",
         desc: "Apple iPhone 12th generation",
         price: 999,
@@ -11,17 +14,122 @@ class CatalogModel{
   ];
 }
 
-class Item{
+class Item {
   //final means this can't be changed
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
-   //curl;y braces m,ake them named parameters means yeh required honge hamesha
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
+   //curly braces m,ake them named parameters means yeh required honge hamesha
 
+  // yaha pr humne string isliye dia because key is in string agar kuch or hota toh dynamic dena tha
+  // we are making our constructor as factory constructor because we ahev written a logic and all variables are final agar final hata do
+  // toh factory ahta do but factory likhna padega toh final likhna padega
+  // this code is for decoding purpose
+  // class se map banaya = decode
+  // factory  Item.fromMap(Map<String, dynamic> map){
+  //     return Item(
+  //         id: map["id"],
+  //         name: map["name"],
+  //         desc: map["desc"],
+  //         price: map["price"],
+  //         color: map["color"],
+  //         image: map["image"]
+  //     );
+  //   }
+
+  // //class se map banayege = encode
+  // //this will match from your backend part abhi we have both front and back mapping same
+  // toMap() => {
+  //       "id": id,
+  //       "name": name,
+  //       "desc": desc,
+  //       "price": price,
+  //       "color": color,
+  //       "image": image
+  // };
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desc: desc ?? this.desc,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'color': color,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      desc: map['desc'] as String,
+      price: map['price'] as num,
+      color: map['color'] as String,
+      image: map['image'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(covariant Item other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.id == id &&
+      other.name == name &&
+      other.desc == desc &&
+      other.price == price &&
+      other.color == color &&
+      other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      name.hashCode ^
+      desc.hashCode ^
+      price.hashCode ^
+      color.hashCode ^
+      image.hashCode;
+  }
 }
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import '../model/catalog.dart';
 import '../widgets/drawer.dart';
 import '../widgets/items_widget.dart';
@@ -19,12 +17,16 @@ class _HomePageState extends State<HomePage> {
   // especially of stateful widgets ki ab start hue, over hue , etc
   // this is a place where widget is not created as build method call is nto made
   @override
-  void initState() {//take the data here and pass irt into build method
+  void initState() {
+    //take the data here and pass irt into build method
     super.initState();
-    loadData();//this will take all data pehle hi widget ke
+    loadData(); //this will take all data pehle hi widget ke
   }
+
   loadData() async {
-    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");//this file will return future means it will take time to retrive
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson = await rootBundle.loadString(
+        "assets/files/catalog.json"); //this file will return future means it will take time to retrive
     //now we have got the json object in form of string but we need in object so we will decode the file
     //after decoding it will give it in form of map (dynamic value) (string to another dynamic format)
     //same way we have encoding (dynamic another format to string)
@@ -34,35 +36,38 @@ class _HomePageState extends State<HomePage> {
     var productData = decodedData["products"];
     //now map this data with catalog model
 
+    //now we need to make a list to get the products to convert it into list of items
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(30, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page",style: const TextStyle(
-        color: Colors.black
-      ),
-      ),
+        title: Text(
+          "Home Page",
+          style: const TextStyle(color: Colors.black),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          },
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) => ItemWidget(
+                  item: CatalogModel.items[index],
+                ),
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
   }
 }
 
-
 // the first curly braces represent json object
 // second product is called as key and it has a array of json objects
-
