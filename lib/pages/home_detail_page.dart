674202853bcf5d0ec19e5ab2db075a945b5ed9ed
roshniverma021 <roshni.verma@thoughtworks.com>
2023-01-10 10:36:32 +0530

    [Roshni] Refactor. Home detail page content

--- lib/pages/home_detail_page.dart
+++ lib/pages/home_detail_page.dart
@@ -15,7 +15,9 @@ class HomeDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
       backgroundColor: MyTheme.creamColor,
       bottomNavigationBar: Container(
         color: Colors.white,
@@ -36,35 +38,41 @@ class HomeDetailPage extends StatelessWidget {
         ).p32(),
       ),
       body: SafeArea(
-          bottom: false,
-          child: Column(
-            children: [
-              Hero(
-                      tag: Key(catalog.id.toString()),
-                      child: Image.network(catalog.image))
-                  .h32(context),
-              Expanded(
-                  child: VxArc(
-                      height: 30.0,
-                      arcType: VxArcType.CONVEY,
-                      edge: VxEdge.TOP,
-                      child: Container(
-                        color: Colors.white,
-                        width: context.screenWidth,
-                        child: Column(children: [
-                          catalog.name.text.xl4
-                              .color(MyTheme.darkBluishColor)
-                              .bold
-                              .make(),
-                          catalog.desc.text
-                              .textStyle(context.captionStyle)
-                              .xl
-                              .make(),
-                          10.heightBox,
-                        ]).p64(),
-                      )))
-            ],
-          )),
+        bottom: false,
+        child: Column(
+          children: [
+            Hero(
+              tag: Key(catalog.id.toString()),
+              child: Image.network(catalog.image),
+            ).h32(context),
+            Expanded(
+                child: VxArc(
+                  height: 30.0,
+                  arcType: VxArcType.CONVEY,
+                  edge: VxEdge.TOP,
+                  child: Container(
+                    color: context.cardColor,
+                    width: context.screenWidth,
+                    child: Column(
+                      children: [
+                        catalog.name.text.xl4
+                            .color(context.accentColor)
+                            .bold
+                            .make(),
+                        catalog.desc.text.textStyle(context.captionStyle).xl.make(),
+                        10.heightBox,
+                        "Dolor sea takimata ipsum sea eirmod aliquyam est. Eos ipsum voluptua eirmod elitr, dolores magna. Amet vero vero vero kasd, dolore sea sed sit invidunt nonumy est sit clita. Diam aliquyam amet tempor diam no aliquyam invidunt. Elitr lorem eirmod dolore clita. Rebum."
+                            .text
+                            .textStyle(context.captionStyle)
+                            .make()
+                            .p16()
+                      ],
+                    ).py64(),
+                  ),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
