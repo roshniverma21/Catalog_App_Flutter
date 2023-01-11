79e2debe7f357930650767ff70024a852e8c1d46
roshniverma021 <roshni.verma@thoughtworks.com>
2023-01-11 10:18:01 +0530

    [Roshni] scaffold messenger in buy button

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -20,8 +20,7 @@ class CartPage extends StatelessWidget {
             Divider(),
             _CartTotal(),
           ],
-        )
-    );
+        ));
   }
 }
 
@@ -36,10 +35,13 @@ class _CartTotal extends StatelessWidget {
           "\Rs.9999".text.xl5.color(context.theme.accentColor).make(),
           30.widthBox,
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: "Buying is not supported yet".text.make()));
+            },
             style: ButtonStyle(
                 backgroundColor:
-                MaterialStateProperty.all(context.theme.buttonColor)),
+                    MaterialStateProperty.all(context.theme.buttonColor)),
             child: "Buy".text.white.make(),
           ).w32(context)
         ],
@@ -69,6 +71,3 @@ class __CartListState extends State<_CartList> {
     );
   }
 }
-
-
-  
